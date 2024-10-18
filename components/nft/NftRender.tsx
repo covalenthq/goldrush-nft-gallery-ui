28ec19424b6be6fafffa1242758e40ba704d5ecf
Karan Pargal <karanpargal007@gmail.com>
2024-10-18 21:44:01 +0530

    fix: Update thumbhash

--- components/nft/NftRender.tsx
+++ components/nft/NftRender.tsx
@@ -1,25 +1,43 @@
 import React from "react"
 import { NftDetailsType } from "@/utils/types/shared.types"
 import { useGoldRush } from "@covalenthq/goldrush-kit"
+import { thumbHashToDataURL } from "thumbhash"
 
 const NftRender: React.FC<{
   nftDetails: NftDetailsType
 }> = ({ nftDetails }) => {
   const { theme } = useGoldRush()
 
-  console.log(nftDetails)
-
   return (
     <div
       className="flex flex-col gap-2 border"
       style={{ borderRadius: `${theme.borderRadius}px` }}
     >
-      
       <img
+        src={thumbHashToDataURL(
+          new Uint8Array(
+            atob(nftDetails.thumbhash)
+              .split("")
+              .map((x) => x.charCodeAt(0))
+          )
+        )}
+        alt={"Token"}
+        loading="lazy"
+        onLoad={(e) => {
+          ;(e.target as HTMLImageElement).src =
+            nftDetails.image_url || nftDetails.thumbhash || ""
+        }}
+        className="max-h-fit h-[500px] w-[500px] object-contain rounded-lg"
+        style={{
+          borderTopLeftRadius: theme.borderRadius,
+          borderTopRightRadius: theme.borderRadius,
+        }}
+      />
+      {/* <img
         src={nftDetails.image_url}
         alt="NFT"
         className="max-h-fit h-[500px] w-[500px] object-contain"
-      />
+      /> */}
     </div>
   )
 }

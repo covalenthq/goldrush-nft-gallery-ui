59487abab012f1dfad8179cbc0d20a3f6bd8a38d
Karan Pargal <83785975+karanpargal@users.noreply.github.com>
2024-10-26 18:12:27 +0530

    feat: Add top collections (#22)
    * feat: Add top collections
    
    * fix: add loading
    
    * fix: Text Changes
    
    * fix: Column

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@
 import { useContext, useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { CHAINS } from "@/utils/constants/chains"
+import { TOP_COLLECTIONS } from "@/utils/constants/collections"
 import { NftContext } from "@/utils/store/NFT.store"
 import { ChainItem, GoldRushClient } from "@covalenthq/client-sdk"
+import { AddressAvatar } from "@covalenthq/goldrush-kit"
 import { LoaderCircleIcon } from "lucide-react"
+import { thumbHashToDataURL } from "thumbhash"
+
 import { COVALENT_API_KEY } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -85,57 +89,104 @@ export default function IndexPage() {
         <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl">
           GoldRush NFT Gallery UI
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground font-medium">
+        <p className="text-lg text-muted-foreground font-medium">
           Accessible and customizable components that you can copy and paste
           into your apps. Free. Open Source. And Next.js 13 Ready.
         </p>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            setLoading(true)
-            router.push(`/collection/${value}/${address}`)
-          }}
-        >
-          <div className="flex flex-col gap-2">
-            <ChainSelector
-              open={open}
-              value={value}
-              chainName={chainName}
-              busy={busy}
-              allChains={allChains}
-              setValue={setValue}
-              setOpen={setOpen}
-              setChainName={setChainName}
-            />
-            <Label htmlFor="contract_address">Contract Address</Label>
-            <Input
-              className="w-[400px]"
-              type="input"
-              id="address"
-              placeholder="Contract Address"
-              value={address}
-              onChange={(e) => {
-                setAddress(e.target.value)
-              }}
-            />
-            <div>
-              <Button
-                disabled={address.length === 0 || !value || busy}
-                type="submit"
-              >
-                {
-                  <div className="flex items-center gap-2">
-                    {loading ? (
-                      <LoaderCircleIcon size={16} className="animate-spin" />
-                    ) : (
-                      "Continue"
+        <div className="flex items-start gap-4">
+          <div className="flex flex-col gap-2 border-r border-secondary-light dark:border-secondary-dark pr-4">
+            <p className="font-medium">Try out some of the top collections.</p>
+            <div className="flex flex-wrap items-center gap-4 max-w-2xl">
+              {TOP_COLLECTIONS.map((collection) => (
+                <button
+                  key={collection.collection_address}
+                  className="flex items-center gap-3 p-2 border border-secondary-light dark:border-secondary-dark rounded-md w-80"
+                  onClick={() => {
+                    setLoading(true)
+                    router.push(
+                      `/collection/${collection.chain_mapping}/${collection.collection_address}`
+                    )
+                  }}
+                >
+                  <img
+                    src={thumbHashToDataURL(
+                      new Uint8Array(
+                        atob(collection.thumbhash)
+                          .split("")
+                          .map((x) => x.charCodeAt(0))
+                      )
                     )}
+                    alt={"Token"}
+                    loading="lazy"
+                    onLoad={(e) => {
+                      ;(e.target as HTMLImageElement).src =
+                        collection.logo || collection.thumbhash || ""
+                    }}
+                    className="h-12 w-12 rounded-xl"
+                  />
+                  <div className="flex flex-col items-start text-left">
+                    <p className="font-semibold text-primary-light dark:text-primary-dark text-sm">
+                      {collection.name}
+                    </p>
+                    <p className="text-xs text-secondary-light dark:text-secondary-dark">
+                      {collection.chain}
+                    </p>
                   </div>
-                }
-              </Button>
+                </button>
+              ))}
             </div>
           </div>
-        </form>
+
+          <div className="flex flex-col gap-2 pl-4">
+            <p className="font-medium ">Enter an NFT Contract Address.</p>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                setLoading(true)
+                router.push(`/collection/${value}/${address}`)
+              }}
+            >
+              <div className="flex flex-col gap-2">
+                <ChainSelector
+                  open={open}
+                  value={value}
+                  chainName={chainName}
+                  busy={busy}
+                  allChains={allChains}
+                  setValue={setValue}
+                  setOpen={setOpen}
+                  setChainName={setChainName}
+                />
+                <Label htmlFor="contract_address">Contract Address</Label>
+                <Input
+                  className="w-[400px]"
+                  type="input"
+                  id="address"
+                  placeholder="Contract Address"
+                  value={address}
+                  onChange={(e) => {
+                    setAddress(e.target.value)
+                  }}
+                />
+                <Button
+                  disabled={address.length === 0 || !value || busy}
+                  type="submit"
+                  className="w-28 mt-2"
+                >
+                  {
+                    <div className="flex items-center gap-2">
+                      {loading ? (
+                        <LoaderCircleIcon size={16} className="animate-spin" />
+                      ) : (
+                        "Continue"
+                      )}
+                    </div>
+                  }
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   )

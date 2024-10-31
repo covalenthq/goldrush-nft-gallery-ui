fd24ab65f90b8648f6b32e96dcc64e4c48b4e018
Karan Pargal <karanpargal007@gmail.com>
2024-11-01 02:10:01 +0530

    nit

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,11 @@
-"use client";
+"use client"
 
-import { PoweredByGoldRush } from "@/components/PoweredByGoldRush";
+import { PoweredByGoldRush } from "@/components/PoweredByGoldRush"
 
 export const Footer: React.FC = () => {
   return (
     <footer className="flex bg-background-light dark:bg-background-dark w-full items-center justify-between gap-4 py-4 px-8 border-t border-secondary-light dark:border-secondary-dark h-16 fixed bottom-0 left-0">
       <div className="flex flex-col gap-4 gap-y-2 sm:flex-row">
-        <a
-          href="https://github.com/covalenthq/goldrush-kit"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-medium leading-none hover:underline"
-        >
-          UI Kit
-        </a>
-
         <a
           href="https://www.goldrush.dev/docs/api/"
           target="_blank"
@@ -22,6 +13,14 @@ export const Footer: React.FC = () => {
           className="font-medium leading-none hover:underline"
         >
           API
+        </a>
+        <a
+          href="https://github.com/covalenthq/goldrush-kit"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium leading-none hover:underline"
+        >
+          UI Kit
         </a>
       </div>
 
@@ -34,5 +33,5 @@ export const Footer: React.FC = () => {
         <PoweredByGoldRush />
       </a>
     </footer>
-  );
-};
+  )
+}
